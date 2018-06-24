b69dd3421a1453e763b54abaa5909e1d43b08d97
Артем Горбачев <meltaus@yandex.ru>
2018-06-24 18:28:19 +0700

    Интерейс, реализующий слушатель соединения

--- network/src/main/java/ru/javacore/gorbachev/network/listener/interfaces/TCPConnectionListener.java
+++ network/src/main/java/ru/javacore/gorbachev/network/listener/interfaces/TCPConnectionListener.java
@@ -1,4 +1,11 @@
 package ru.javacore.gorbachev.network.listener.interfaces;
 
+import ru.javacore.gorbachev.network.connection.TCPConnection;
+
 public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+    void onReciveMsg(TCPConnection tcpConnection, String value);
+    void onDisconect(TCPConnection tcpConnection);
+    void onExeption(TCPConnection tcpConnection, Exception e);
 }
