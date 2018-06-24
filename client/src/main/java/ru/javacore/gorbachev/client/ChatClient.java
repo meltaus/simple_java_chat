9cf747ed92f277945f758426f60025e23b532955
Артем Горбачев <meltaus@yandex.ru>
2018-06-24 17:46:01 +0700

    Методы запсука для клиента и сервера.

--- client/src/main/java/ru/javacore/gorbachev/client/ChatClient.java
+++ client/src/main/java/ru/javacore/gorbachev/client/ChatClient.java
@@ -1,4 +1,8 @@
 package ru.javacore.gorbachev.client;
 
 public class ChatClient {
+
+    public static void main (String[] args) {
+
+    }
 }
--- server/src/main/java/ru/javacore/gorbachev/server/ChatServer.java
+++ server/src/main/java/ru/javacore/gorbachev/server/ChatServer.java
@@ -1,4 +1,8 @@
 package ru.javacore.gorbachev.server;
 
 public class ChatServer {
+    
+    public static void main(String[] args) {
+
+    }
 }
