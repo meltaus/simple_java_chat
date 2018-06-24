d03e9b79499649396833296962edc5d2cbfa62d1
Артем Горбачев <meltaus@yandex.ru>
2018-06-24 18:27:20 +0700

    Реализация потока слушателя сокета. обработка принятия сообщения из потока

--- network/src/main/java/ru/javacore/gorbachev/network/listener/ListenSocket.java
+++ network/src/main/java/ru/javacore/gorbachev/network/listener/ListenSocket.java
@@ -1,4 +1,7 @@
-package ru.javacore.gorbachev.network.Listener;
+package ru.javacore.gorbachev.network.listener;
+
+import ru.javacore.gorbachev.network.connection.TCPConnection;
+import ru.javacore.gorbachev.network.listener.interfaces.TCPConnectionListener;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,19 +9,25 @@ import java.io.IOException;
 public class ListenSocket implements Runnable {
 
     private final BufferedReader inMsg;
+    private final TCPConnectionListener listenerEvent;
+    private final TCPConnection tcpConnection;
 
-    public ListenSocket(BufferedReader in) {
+    public ListenSocket(BufferedReader in, TCPConnectionListener tcpConnectionListener, TCPConnection tcpConnection) {
+        this.listenerEvent = tcpConnectionListener;
         this.inMsg = in;
+        this.tcpConnection = tcpConnection;
     }
 
     public void run() {
         try {
-            String msg = this.inMsg.readLine();
+            while (true) {
+                this.listenerEvent.onReciveMsg(this.tcpConnection, this.inMsg.readLine());
+            }
         } catch (IOException e) {
             //Ошибка чтения из потока
             e.printStackTrace();
         } finally {
-            
+
         }
     }
 }
