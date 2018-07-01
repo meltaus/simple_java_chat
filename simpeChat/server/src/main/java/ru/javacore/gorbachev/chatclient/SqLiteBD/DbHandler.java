80accd27e4fa73fa49326ec5d214278685dbc8c2
Артем Горбачев <meltaus@yandex.ru>
2018-07-01 19:03:33 +0700

    Работа с БД. Получить все записи Добавить запись, удалить запись, Получить одну запись по никнейму.

--- simpeChat/server/src/main/java/ru/javacore/gorbachev/chatclient/SqLiteBD/DbHandler.java
+++ simpeChat/server/src/main/java/ru/javacore/gorbachev/chatclient/SqLiteBD/DbHandler.java
@@ -1,4 +1,107 @@
 package ru.javacore.gorbachev.chatclient.SqLiteBD;
 
+import org.sqlite.JDBC;
+import ru.javacore.gorbachev.chatclient.essence.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class DbHandler {
+
+    // Константа, в которой хранится адрес подключения
+    private static final String CON_STR = "jdbc:sqlite:../simplechat.db";
+
+    // Используем шаблон одиночка, чтобы не плодить множество
+    // экземпляров класса DbHandler
+    private static DbHandler instance = null;
+
+    public static synchronized DbHandler getInstance() throws SQLException {
+        if (instance == null)
+            instance = new DbHandler();
+        return instance;
+    }
+
+
+    // Объект, в котором будет храниться соединение с БД
+    private Connection connection;
+
+    private DbHandler() throws SQLException {
+        // Регистрируем драйвер, с которым будем работать
+        // в нашем случае Sqlite
+        DriverManager.registerDriver(new JDBC());
+        // Выполняем подключение к базе данных
+        this.connection = DriverManager.getConnection(CON_STR);
+    }
+
+    public List<User> getAllUsers() {
+
+        // Statement используется для того, чтобы выполнить sql-запрос
+        try (Statement statement = this.connection.createStatement()) {
+            // В данный список будем загружать наши продукты, полученные из БД
+            List<User> users = new ArrayList<User>();
+            // В resultSet будет храниться результат нашего запроса,
+            // который выполняется командой statement.executeQuery()
+            ResultSet resultSet = statement.executeQuery("SELECT id, nickName, password FROM user_chat");
+            while (resultSet.next()) {
+                users.add(new User(resultSet.getInt("id"),
+                        resultSet.getString("nickName"),
+                        resultSet.getString("password")));
+            }
+            // Возвращаем наш список
+            return users;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Если произошла ошибка - возвращаем пустую коллекцию
+            return Collections.emptyList();
+        }
+    }
+
+    public List<User> getUser (final String nickName){
+        try (Statement statement = this.connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT id, nickName, password FROM user_chat " +
+                    "WHERE nickName = '" + nickName + "'");
+            List<User> users = new ArrayList<User>();
+            while (resultSet.next()) {
+                users.add(new User(resultSet.getInt("id"),
+                        resultSet.getString("nickName"),
+                        resultSet.getString("password")));
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Если произошла ошибка - возвращаем пустую коллекцию
+            return Collections.emptyList();
+        }
+    }
+
+    // Добавление пользователя в БД
+    public void addUser(User user) {
+        // Создадим подготовленное выражение, чтобы избежать SQL-инъекций
+        try (PreparedStatement statement = this.connection.prepareStatement(
+                "INSERT INTO user_chat(`nickName`, `password`) " +
+                        "VALUES(?, ?)")) {
+            statement.setObject(1, user.getNickName());
+            statement.setObject(2, user.getPassword());
+            // Выполняем запрос
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Удаление пользователя по id
+    public void deleteUser(int id) {
+        try (PreparedStatement statement = this.connection.prepareStatement(
+                "DELETE FROM user_chat WHERE id = ?")) {
+            statement.setObject(1, id);
+            // Выполняем запрос
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
