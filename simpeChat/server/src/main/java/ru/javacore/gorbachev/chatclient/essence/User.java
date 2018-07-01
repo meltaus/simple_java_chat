ed97de0d04ae94eb2b0099a3bd2890ef03301969
Артем Горбачев <meltaus@yandex.ru>
2018-07-01 19:02:48 +0700

    Один зарегистрированный пользователь

--- simpeChat/server/src/main/java/ru/javacore/gorbachev/chatclient/essence/User.java
+++ simpeChat/server/src/main/java/ru/javacore/gorbachev/chatclient/essence/User.java
@@ -1,4 +1,34 @@
 package ru.javacore.gorbachev.chatclient.essence;
 
 public class User {
+    private final int id;
+    private final String nickName;
+    private final String password;
+
+    public User(int id, String nickName, String password) {
+        this.id = id;
+        this.nickName = nickName;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nickName='" + nickName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
