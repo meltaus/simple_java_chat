835b75ad60517951ce7364a87aebb9af6b1e4361
Артем Горбачев <meltaus@yandex.ru>
2018-07-01 23:05:31 +0700

    Окно "О программе"

---
+++ simpeChat/client/src/main/java/ru/javacore/gorbachev/chatclient/controller/AboutController.java
@@ -0,0 +1,29 @@
+package ru.javacore.gorbachev.chatclient.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class AboutController {
+
+    private Stage aboutStage;
+
+    //Метод вызываемый при отрисовки формы
+    @FXML
+    public void initialize() {
+    }
+
+    public void aboutStage(Stage mainStage) {
+        this.aboutStage = mainStage;
+    }
+
+    //Выход
+    public void onExit(ActionEvent actionEvent) {
+        Node source = (Node)actionEvent.getSource();
+        Stage stage = (Stage)source.getScene().getWindow();
+        stage.close();
+    }
+}
