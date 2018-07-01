75541ce431183c2e66f7ebfe5fc08a3266fa9491
Артем Горбачев <meltaus@yandex.ru>
2018-07-01 20:42:16 +0700

    Регистрация и авторизация пользователя

--- simpeChat/server/src/main/java/ru/javacore/gorbachev/chatclient/server/ChatServer.java
+++ simpeChat/server/src/main/java/ru/javacore/gorbachev/chatclient/server/ChatServer.java
@@ -1,12 +1,16 @@
 package ru.javacore.gorbachev.chatclient.server;
 
+import ru.javacore.gorbachev.chatclient.SqLiteBD.DbHandler;
+import ru.javacore.gorbachev.chatclient.essence.User;
 import ru.javacore.gorbachev.chatclient.network.connection.TCPConnection;
 import ru.javacore.gorbachev.chatclient.essence.UserChat;
 import ru.javacore.gorbachev.chatclient.network.listener.TCPConnectionListener;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ChatServer implements TCPConnectionListener {
     private static final int PORT = 8189;
@@ -15,10 +19,27 @@ public class ChatServer implements TCPConnectionListener {
     private final ArrayList<TCPConnection> connections;
     //Список всех пользователей чата
     private final ArrayList<UserChat> userChat = new ArrayList<>();
+    //Инстанс подключения к БД
+    private final DbHandler dbHandler;
 
     public ChatServer() {
         this.connections = new ArrayList<>();
         System.out.println("Сервер запущен");
+        try {
+            // Создаем экземпляр по работе с БД
+            dbHandler = DbHandler.getInstance();
+            // Добавляем запись
+            //dbHandler.addProduct(new Product("Музей", 200, "Развлечения"));
+            // Получаем все записи и выводим их на консоль
+//            List<User> users = dbHandler.getAllUsers();
+//            for (User user : users) {
+//                System.out.println(user.toString());
+//            }
+            // Удаление записи с id = 8
+            //dbHandler.deleteProduct(8);
+        } catch (SQLException e) {
+            System.out.println("Подключение к БД завершилось ошибкой");
+        }
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             while (true) {
                 try {
@@ -52,20 +73,51 @@ public class ChatServer implements TCPConnectionListener {
         this.connections.add(tcpConnection);
     }
 
+    //Прием сообщения. В случае приема ключа обрабатываем его
     public synchronized void onReciveMsg(TCPConnection tcpConnection, String value) {
         System.out.println(value);
-        if (value.contains("/MyNickName")){
+        if (value.contains("/MyNickName")){ //Представление серверу
             value = value.replace("/MyNickName ", "");
             setUserChat(value, tcpConnection);
-        } else if (value.contains("/ReplaceMyNickName")) {
+        } else if (value.contains("/ReplaceMyNickName")) { //Смена ника
             value = value.replace("/ReplaceMyNickName", "");
             String[] nick = value.split(" - ");
             nick[0] = nick[0].replace(" ", "");
             nick[1] = nick[1].replace(" ", "");
             replraseUserChat(nick, tcpConnection);
-        } else if (value.contains("/AllNicks")) {
+        } else if (value.contains("/AllNicks")) { //Вернуть все никнеймы
             sendAllUserChat();
-        } else if (value.contains("/w")) {
+        } else if (value.contains("/Register")){ //Зарегистрировать пользователя
+            value = value.replace("/Register ", "");
+            String[] nick = value.split(" ", 3);
+            nick[0] = nick[0].replace(":", "");
+            nick[1] = nick[1].replace(" ", "");
+            nick[2] = nick[2].replace(" ", "");
+            List<User> users = dbHandler.getUser(nick[1]);
+            if (users.size() < 1) {
+                dbHandler.addUser(new User(nick[1], nick[2]));
+                tcpConnection.sendMsg("Вы успешно зарегистрированны");
+            } else {
+                tcpConnection.sendMsg("Недопустимое имя пользователя");
+            }
+        } else if (value.contains("/AuthUser")) {
+            value = value.replace("/AuthUser ", "");
+            String[] nick = value.split(" ", 3);
+            nick[0] = nick[0].replace(":", "");
+            nick[1] = nick[1].replace(" ", "");
+            nick[2] = nick[2].replace(" ", "");
+            List<User> users = dbHandler.getUser(nick[1]);
+            if (users.size() == 1) {
+                User user = users.get(0);
+                if (user.getNickName().equals(nick[1]) && user.getPassword().equals(nick[2])) {
+                    tcpConnection.sendMsg("Вы усешно авторизованны");
+                    tcpConnection.sendMsg("/AuthUser true");
+                } else {
+                    tcpConnection.sendMsg("Авторизация завершилась ошибкой");
+                    tcpConnection.sendMsg("/AuthUser false");
+                }
+            }
+        } else if (value.contains("/w")) { //Личное сообщение
             String workString = value.replace("/w ", "");
             String[] nick = workString.split(" ");
             value = value.replace("/w " + nick[1],"");
@@ -79,7 +131,7 @@ public class ChatServer implements TCPConnectionListener {
             if (index > -1) {
                 sendCurrnetUser(value, this.connections.get(index));
             }
-        } else {
+        } else { //Разослать всем
             sendAll(value);
         }
 
